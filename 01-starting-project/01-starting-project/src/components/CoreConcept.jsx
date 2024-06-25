f226cfde9dbec03bd192cfd608d4aa24e0758a2d
Francisco Pelaez <pelaez.francisco@gmail.com>
2024-06-25 11:49:28 -0500

    First commit

---
+++ 01-starting-project/01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,10 @@
+//function CoreConcept(props) {
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title}></img>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
